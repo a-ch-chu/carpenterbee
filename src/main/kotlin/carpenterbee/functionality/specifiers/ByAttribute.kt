8e97b2adbb5a3840836de940d5b2672fbfbf032e
Alston Chu <Alston Chu@Fregat>
2020-02-10 17:15:07 +1100

    Minor: string interpolation cleanup

--- src/main/kotlin/carpenterbee/functionality/specifiers/ByAttribute.kt
+++ src/main/kotlin/carpenterbee/functionality/specifiers/ByAttribute.kt
@@ -7,6 +7,6 @@ import org.openqa.selenium.By
 
 public fun Specifiers.attribute(key: String, value: Any?) = ByAttribute(key, value)
 
-public class ByAttribute(val key: String, val value: Any?) : By.ByCssSelector("[$key='${value.toString()}']") {
+public class ByAttribute(val key: String, val value: Any?) : By.ByCssSelector("[$key='$value']") {
     public override fun toString(): String = "By.attribute $key = $value"
 }
