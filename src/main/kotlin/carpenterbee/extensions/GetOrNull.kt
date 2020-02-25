804e4996a2617f4cc8b6a6db3949f9e2266aa4e6
Alston Chu <Alston Chu@Fregat>
2020-02-25 16:27:19 +1100

    Minor clarification

--- src/main/kotlin/carpenterbee/extensions/GetOrNull.kt
+++ src/main/kotlin/carpenterbee/extensions/GetOrNull.kt
@@ -7,7 +7,7 @@ import carpenterbee.*
 import carpenterbee.functionality.HasParent
 
 public fun SearchContext?.getOrNull(specifier: By): WebElement? =
-    this?.let { findElements(specifier).firstOrNull() }
+    this?.run { findElements(specifier).firstOrNull() }
 
 public fun HasParent<*>.getOrNull(): WebElement? =
     parent.scope.getOrNull(specifier)
