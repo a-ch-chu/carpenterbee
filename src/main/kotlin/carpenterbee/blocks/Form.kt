749366973c8699398780336967611c22fd914339
Alston Chu <Alston Chu@Fregat>
2020-02-26 10:50:37 +1100

    Cleanup & fix

--- src/main/kotlin/carpenterbee/blocks/Form.kt
+++ src/main/kotlin/carpenterbee/blocks/Form.kt
@@ -11,7 +11,7 @@ public open class Form<TParent : Block>(parent: TParent, specifier: By) : Sectio
         get() = tag.getAttribute("target")
 
     public val data: Iterable<Pair<String, String>>
-        get() = tag.executeScript<Array<Array<String>>>(
-            "return Array.from(new FormData(arguments[0]));"
-        ).map { it[0] to it[1] }
+        get() = tag.let {
+            it.executeScript<Array<Array<String>>>("return Array.from(new FormData(arguments[0]));", it)
+        }.map { it[0] to it[1] }
 }
