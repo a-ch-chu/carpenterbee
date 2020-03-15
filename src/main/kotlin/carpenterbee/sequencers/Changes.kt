3a0414b2a0b4dcd00eeb20e2333e3c1f3a1e06cb
Alston Chu <Alston Chu@Fregat>
2020-03-16 10:57:26 +1100

    Change sequencer cleanup

--- src/main/kotlin/carpenterbee/sequencers/Changes.kt
+++ src/main/kotlin/carpenterbee/sequencers/Changes.kt
@@ -3,7 +3,7 @@
 
 package carpenterbee.sequencers
 
-import carpenterbee.WebElement
+import carpenterbee.*
 import carpenterbee.functionality.waiting.Wait
 import org.openqa.selenium.StaleElementReferenceException
 
@@ -13,12 +13,23 @@ public class Changes(
 ) : Sequencer {
     private lateinit var stored: WebElement
 
+    public constructor(element: Control<*, *>, wait: Wait = Wait())
+            : this({ element.read { this } }, wait)
+
+    public constructor(element: Section<*>, wait: Wait = Wait())
+            : this({ element.tag }, wait)
+
     public override fun preInteract(tag: WebElement) {
         stored = readTag?.invoke() ?: tag
     }
 
     public override fun postInteract(tag: WebElement) {
-        if (!wait.until { ::stored.isInitialized && stored.stale })
+        if (!::stored.isInitialized)
+            throw SequencerException(
+                "Tried to call post-interaction sequencing before pre-interaction."
+            )
+
+        if (!wait.until { stored.stale })
             throw SequencerException(
                 "Timed out waiting for DOM mutations to occur after interaction."
             )
